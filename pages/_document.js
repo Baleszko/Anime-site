a88658ae38218fc9f73063769fc5f5c5ed91f700
Bálint Tóth <72336409+Baleszko@users.noreply.github.com>
2023-01-30 15:59:04 +0100

    Bug Fix

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title> Anime Site</title>
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700;800&display=swap"
             rel="stylesheet"
           />
+          <link rel="icon" type="image/x-icon" href="/public/favicon.ico" />
         </Head>
         <body>
           <Main />
