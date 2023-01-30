b95296f75f3ede40874f4398cdf99bdd0cb089be
Bálint Tóth <72336409+Baleszko@users.noreply.github.com>
2023-01-30 15:47:47 +0100

    Bug Fix

--- components/Error.jsx
+++ components/Error.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import style from "../styles/Error.module.css";
+import errorSvg from "../public/error.svg";
+
 function Error() {
   return (
     <div className={style.errorPage}>
       <div className={style.errorBox}>
-        <Image src="/error.svg" alt="Error" height={200} width={200} />
+        <Image src={errorSvg} alt="Error" height={200} width={200} />
         <h2>Something went wrong</h2>
         <h2>Try Again!</h2>
       </div>
